c131386d0d97355697cf21e25780f4fd245b0318
Oliver Zheng <gitcommits@oliverzheng.com>
2016-07-29 21:54:17 -0700

    Initial Commit

---
+++ Demeter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demeter
+//
+//  Created by Oliver Zheng on 7/29/16.
+//  Copyright © 2016 Oliver Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
